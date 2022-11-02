2f99ad7f50f92ffe907d129bb979f46de9e7afce
Cedrus81 <erez.eitan.97@gmail.com>
2022-11-02 17:59:17 +0200

    fixed save loc

--- js/app.controller.js
+++ js/app.controller.js
@@ -6,7 +6,7 @@ window.onAddMarker = onAddMarker
 window.onPanTo = onPanTo
 window.onGetLocs = renderLocs
 window.onGetUserPos = onGetUserPos
-window.onSaveLoc = onSaveLoc
+window.onAddLoc = onAddLoc
 window.onDeleteLoc = onDeleteLoc
 
 function onInit() {
@@ -69,7 +69,7 @@ function onDeleteLoc(name) {
     locService.deleteLoc(name)
 
 }
-function onSaveLoc(ev, elForm, lat, lng) {
+function onAddLoc(ev, elForm, lat, lng) {
     ev.preventDefault()
     const name = elForm.querySelector('input').value
     locService.addLoc({ lat, lng }, name)
